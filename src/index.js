cd642e6d879829abdf684b62aea2d8203f8615f0
Jim Grimes <jimgrimes86@gmail.com>
2023-09-18 10:53:55 -0400

    DONE: added toys to webpage

--- src/index.js
+++ src/index.js
@@ -13,3 +13,32 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+const toyDatabase = 'http://localhost:3000/toys';
+
+fetch(toyDatabase)
+.then((response) => response.json())
+.then(toyPopulator)
+
+function toyPopulator(toyObject) {
+  const toyCollection = document.getElementById('toy-collection');
+  toyObject.forEach((element) => {
+    const card = document.createElement('div');
+    card.setAttribute('class', 'card');
+
+    const toyName = document.createElement('h2');
+    const toyImage = document.createElement('img');
+    toyImage.setAttribute('class', 'toy-avatar');
+    const toyLikes = document.createElement('p');
+    const button = document.createElement('button');
+
+    card.append(toyName, toyImage, toyLikes, button);
+
+    toyName.textContent = element.name;
+    toyImage.src = element.image;
+    toyLikes.textContent = element.likes;
+    button.textContent = 'like';
+
+    toyCollection.append(card);
+  });
+}
